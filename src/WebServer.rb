4cbc077a949ba7151110a31bc31e032196018233
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2022-11-06 12:41:06 -0600

    Update WebServer.rb

--- src/WebServer.rb
+++ src/WebServer.rb
@@ -1,12 +1,22 @@
 # WebServer.rb
 #run with -> ruby -I . -w WebServer.rb
 require 'sinatra'
-
+require 'faraday'
+require 'json'
+URLpreguntas = 'https://cyxpvpvfbqyudf6ub33ejitqfy0rnwcy.lambda-url.us-east-1.on.aws/preguntas'
+enable :sessions
 get '/' do
-  @cantidadPreguntas = 5
+  session[:cantidadPreguntas] = 5
+  @cantidadPreguntas = session[:cantidadPreguntas]
   erb :index
 end
 
 get '/preguntas' do
+  @cantidadPreguntas = session[:cantidadPreguntas]
+  response = Faraday.get(URLpreguntas)
+  @info = []
+  if response.success?
+    @info = JSON.parse(response.body)
+  end
   erb :preguntas
 end
